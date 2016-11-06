f6b5a09a5a994fdbcdb3b7ef248d75abe98a8f41
Joy Holowicki <joyholowicki@gmail.com>
2016-11-06 15:31:51 -0600

    revision work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,9 +15,65 @@ $(document).ready(function() {
     var finn = new player("Finn", healthPoints = 120, attackPower = 13, counterAttackPower = 10, baseAttackPower = 13);
     var generalHux = new player("General Hux", healthPoints = 130, attackPower = 10, counterAttackPower = 20, baseAttackPower = 10);
 
-    var moveCharacter = function() {
-        $("#Finn").appendTo("Stage-2");
-    };
-    moveCharacter();
 
+    var rpgGame = {
+        selectedPlayer: null,
+        opponent: null,
+        wins: 0,
+        insertPlayer: 0,
+        insertOpponent: 0,
+    };
+    console.log(rpgGame);
+    //inserts selected player into rpgGame and sends enemies to enemy section.
+    $('.players').on('click', function() {
+        if (rpgGame.selectedPlayer === null && rpgGame.opponent === null) {
+            if ($(this).text() === ' Rey') {
+                rpgGame.selectedPlayer = rey;
+                console.log('hi', rpgGame);
+                $('#Finn').appendTo('#Stage-2');
+                $('#General-Hux').appendTo('#Stage-2');
+                $('#Kylo-Ren').appendTo('#Stage-2');
+            } else if ($(this).text() === ' Finn') {
+                rpgGame.selectedPlayer = finn;
+                console.log(rpgGame);
+                $('#Rey').appendTo('#Stage-2');
+                $('#General-Hux').appendTo('#Stage-2');
+                $('#Kylo-Ren').appendTo('#Stage-2');
+            } else if ($(this).text() === ' General Hux') {
+                rpgGame.selectedPlayer = generalHux;
+                console.log(rpgGame);
+                $('#Rey').appendTo('#Stage-2');
+                $('#Finn').appendTo('#Stage-2');
+                $('#Kylo-Ren').appendTo('#Stage-2');
+            } else if ($(this).text() === ' Kylo Ren') {
+                rpgGame.selectedPlayer = kyloRen;
+                console.log(rpgGame);
+                $('#Rey').appendTo('#Stage-2');
+                $('#General-Hux').appendTo('#Stage-2');
+                $('#Finn').appendTo('#Stage-2');
+            }
+        }
+    });
+    //inserts selected opponent into rpgGame and sends opponent to defender section.
+    $('.players').on('click', function() {
+        if ($(this).text() === ' Rey') {
+            rpgGame.opponent = rey;
+            console.log(rpgGame);
+            $('#Rey').appendTo('#Stage-4');
+        } else if ($(this).text() === ' Finn') {
+            rpgGame.opponent = finn;
+            console.log('yo', rpgGame);
+            $('#Finn').appendTo('#Stage-4');
+        } else if ($(this).text() === ' General Hux') {
+            rpgGame.opponent = generalHux;
+            console.log(rpgGame);
+            $('#General-Hux').appendTo('#Stage-4');
+        } else if ($(this).text() === ' Kylo Ren') {
+            rpgGame.opponent = kyloRen;
+            console.log(rpgGame);
+            $('#General-Hux').appendTo('#Stage-4');
+        }
+    });
+    //attacks opponent and immediate counter attack by opponent.
+    $('').on('click', )
 });
